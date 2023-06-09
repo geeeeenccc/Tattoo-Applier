a08fe4927c15dd53e631ba9cbae20e4f5d4b2299
Герай Генч <101811004+geeeeenccc@users.noreply.github.com>
2023-06-09 20:27:15 +0300

    Update test_left_eye.py

--- test_left_eye.py
+++ test_left_eye.py
@@ -1,3 +1,32 @@
-def test_placeholder():
-    pass
+import cv2
+import pytest
+from left_eye import main
 
+def test_main_function_runs_without_error():
+    # Ensure that the main function runs without raising any exceptions
+    main()
+
+def test_capture_returns_frame():
+    cap = cv2.VideoCapture(0)
+    ret, frame = cap.read()
+    cap.release()
+    assert ret is True
+
+# @pytest.fixture(scope="module")
+# def window():
+#     import PySimpleGUI as sg
+
+#     layout = [
+#         [sg.Text("Tattoo APPlier", size=(60, 1), justification="center")],
+#         [sg.Button("Exit", size=(10, 1))],
+#     ]
+
+#     window = sg.Window("OpenCV Integration Test", layout)
+#     yield window
+#     window.close()
+
+# def test_window_creation(window):
+#     assert window is not None
+
+# def test_window_closing(window):
+#     assert window.finalize() is None
